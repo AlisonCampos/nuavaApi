7248d2e688a784e18e46885b2666f368d234d83a
AlisonCampos <alisaguiilar090604@gmail.com>
2024-03-21 12:42:42 -0600

    start CORS

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import indexRoutes from './routes/index.routes.js'
 
 const app = express()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use(express.json())
 
